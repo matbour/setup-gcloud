ad00a72fb9a45518cd2f9feabe202b322f185a22
Mathieu Bour <mathieu.tin.bour@gmail.com>
2019-11-18 11:12:45 +0100

    feat: silent curl

--- src/download.ts
+++ src/download.ts
@@ -6,21 +6,21 @@ import * as exec from '@actions/exec';
 export class Download {
     private readonly BASE_URL: string = 'https://dl.google.com/dl/cloudsdk/channels/rapid';
     private sdkUrl: string = this.BASE_URL;
-    public compressMode: 'zip' | 'tar.gz' = 'tar.gz';
 
     constructor(private version: string) {
         this.setSdkDownloadUrl();
     }
 
     async download(): Promise<string> {
-        const destination = resolve(process.cwd(), `google-cloud-sdk.${this.compressMode}`);
+        const extension = this.sdkUrl.endsWith('.zip') ? 'zip' : 'tar.gz';
+        const destination = resolve(process.cwd(), `google-cloud-sdk.${extension}`);
 
         if (existsSync(destination)) {
             return destination;
         }
 
         core.debug(`Downloading ${this.sdkUrl}`);
-        await exec.exec(`curl -o ${destination} ${this.sdkUrl}`);
+        await exec.exec(`curl -S -o ${destination} ${this.sdkUrl}`);
         core.debug(`Downloaded ${this.sdkUrl}`);
 
         return destination;
@@ -30,21 +30,16 @@ export class Download {
         if (this.version === 'latest') {
             if (process.platform === 'win32') {
                 this.sdkUrl = `${this.BASE_URL}/google-cloud-sdk.zip`;
-                this.compressMode = 'zip';
             } else {
                 this.sdkUrl = `${this.BASE_URL}/google-cloud-sdk.tar.gz`;
-                this.compressMode = 'tar.gz';
             }
         } else {
             if (process.platform === 'win32') {
                 this.sdkUrl = `${this.BASE_URL}/downloads/google-cloud-sdk-${this.version}-windows-x86_64.zip`;
-                this.compressMode = 'zip';
             } else if (process.platform === 'darwin') {
                 this.sdkUrl = `${this.BASE_URL}/downloads/google-cloud-sdk-${this.version}-darwin-x86_64.tar.gz`;
-                this.compressMode = 'tar.gz';
             } else {
                 this.sdkUrl = `${this.BASE_URL}/downloads/google-cloud-sdk-${this.version}-linux-x86_64.tar.gz`;
-                this.compressMode = 'tar.gz';
             }
         }
     }
