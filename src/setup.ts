8c59b454219c998569df032163a804a11590b8b6
Mathieu Bour <mathieu.tin.bour@gmail.com>
2019-11-20 11:44:14 +0100

    feat: add try/catch around the setup exec

--- src/setup.ts
+++ src/setup.ts
@@ -6,15 +6,21 @@ import * as exec from '@actions/exec';
 export async function setup() {
     const installScriptExtension = isWindows() ? 'bat' : 'sh';
     const installScript = resolve(getCloudSDKFolder(), `install.${installScriptExtension}`);
-    let args: string;
+    let args: string[];
 
     if (isWindows()) {
-        args = '/S /D=' + getCloudSDKFolder() + '/singleuser /noreporting /nostartmenu /nodesktop';
+        args = ['/S', `/D=${getCloudSDKFolder()}`, '/singleuser', '/noreporting', '/nostartmenu', '/nodesktop'];
     } else {
-        args = '--path-update=false --usage-reporting=false --command-completion=false';
+        args = ['--path-update=false', '--usage-reporting=false', '--command-completion=false'];
     }
 
-    await exec.exec(`${installScript} ${args}`);
+    try {
+        await exec.exec(installScript, args);
+    } catch (e) {
+        core.error('Error during Google Cloud SDK setup!');
+        core.error(e.message);
+        process.exit(1);
+    }
 
     const binPath = resolve(getCloudSDKFolder(), 'bin');
     core.addPath(binPath);
