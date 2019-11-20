d8c45e2f1880001a10df319e0ca1c9e88893060b
Mathieu Bour <mathieu.tin.bour@gmail.com>
2019-11-20 13:47:30 +0100

    fix(ubuntu): install in the /github directory

--- src/utils.ts
+++ src/utils.ts
@@ -17,7 +17,7 @@ export function getCloudSDKFolder(): string {
     if (isWindows()) {
         return 'C:\\Program Files\\Google Cloud SDK';
     } else if (isUbuntu()) {
-        return '/usr/lib/google-cloud-sdk-custom';
+        return '/github/google-cloud-sdk';
     } else {
         const home = process.env.HOME ? process.env.HOME : process.cwd();
         return resolve(home, 'google-cloud-sdk');
