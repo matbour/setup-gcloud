2bd3a5a99747d05edee1ec5dc6a94b67a083d5dc
Mathieu Bour <mathieu.tin.bour@gmail.com>
2019-11-20 11:59:12 +0100

    fix: extract the gcloud sdk at the right path

--- src/download.ts
+++ src/download.ts
@@ -1,16 +1,18 @@
 import * as tc from '@actions/tool-cache';
 import {getCloudSDKFolder, getDownloadLink} from './utils';
 import {mkdirSync} from 'fs';
+import {resolve} from 'path';
 
 export async function download() {
     const downloadLink = getDownloadLink();
     const downloadPath = await tc.downloadTool(downloadLink);
+    const extractionPath = resolve(getCloudSDKFolder(), '..');
 
     mkdirSync(getCloudSDKFolder());
 
     if (downloadLink.endsWith('.zip')) {
-        await tc.extractZip(downloadPath, getCloudSDKFolder());
+        await tc.extractZip(downloadPath, extractionPath);
     } else if (downloadLink.endsWith('.tar.gz')) {
-        await tc.extractTar(downloadPath, getCloudSDKFolder());
+        await tc.extractTar(downloadPath, extractionPath);
     }
 }
