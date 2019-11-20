7771a2ba5ad0768f77b092f50d784bcce17cefc8
Mathieu Bour <mathieu.tin.bour@gmail.com>
2019-11-20 11:36:52 +0100

    fix: mkdir the cloud sdk folder

--- src/download.ts
+++ src/download.ts
@@ -1,12 +1,12 @@
-import * as core from '@actions/core';
-import * as exec from '@actions/exec';
 import * as tc from '@actions/tool-cache';
-import {getCloudSDKFolder, getDownloadLink, isMacOS, isWindows} from './utils';
-import {resolve} from "path";
+import {getCloudSDKFolder, getDownloadLink} from './utils';
+import {mkdirSync} from 'fs';
 
 export async function download() {
     const downloadLink = getDownloadLink();
     const downloadPath = await tc.downloadTool(downloadLink);
+
+    mkdirSync(getCloudSDKFolder());
 
     if (downloadLink.endsWith('.zip')) {
         await tc.extractZip(downloadPath, getCloudSDKFolder());
