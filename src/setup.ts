5a03c0b4f6ade8b8fb47d6b00df5ca7699227f8d
Mathieu Bour <mathieu.tin.bour@gmail.com>
2019-11-20 13:03:17 +0100

    feat: use execSync instead of @actions/exec

--- src/setup.ts
+++ src/setup.ts
@@ -2,36 +2,23 @@ import {getCloudSDKFolder, isWindows} from './utils';
 import {resolve} from "path";
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
+import {execSync} from 'child_process';
 
 export async function setup() {
     const installScriptExtension = isWindows() ? 'bat' : 'sh';
     const installScript = resolve(getCloudSDKFolder(), `install.${installScriptExtension}`);
-    let args: string[];
-
-    if (isWindows()) {
-        // args = ['/S', `/D=${getCloudSDKFolder()}`, '/singleuser', '/noreporting', '/nostartmenu', '/nodesktop'];
-        args = [
-            '--usage-reporting=false',
-            '--command-completion=false',
-            '--path-update=false',
-            '--usage-reporting=false',
-            // '--additional-components',
-            '--quiet'
-        ];
-    } else {
-        args = [
-            '--usage-reporting=false',
-            '--command-completion=false',
-            '--path-update=false',
-            '--usage-reporting=false',
-            // '--additional-components',
-            '--quiet'
-        ];
-    }
+    const args = [
+        '--usage-reporting=false',
+        '--command-completion=false',
+        '--path-update=false',
+        '--usage-reporting=false',
+        // '--additional-components',
+        '--quiet'
+    ];
 
     try {
         if (isWindows()) {
-            await exec.exec(`${installScript} ${args.join(' ')}`);
+            execSync(`${installScript} ${args.join(' ')}`, {stdio: 'inherit'});
         } else {
             await exec.exec(installScript, args);
         }
